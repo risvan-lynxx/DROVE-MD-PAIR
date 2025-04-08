2b02a1598e8e0d2f83bb378bb145fda7ae1a0c4b
lLynx <risvanlynx777@gmail.com>
2025-04-08 15:51:58 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,8 +1,8 @@
 import * as mega from 'megajs';
 
 const auth = {
-    email: 'moyemoye43200@gmail.com',
-    password: 'SILENTLOVER432#',
+    email: 'klwhiteser@gmail.com',
+    password: 'KL WHITE SER@123',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
